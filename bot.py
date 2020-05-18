28318c07ee1d6565b7dc547fec0f8192409acf7f
runo280 <runo280@users.noreply.github.com>
2020-05-18 11:08:13 +0430

    first commit

--- bot.py
+++ bot.py
@@ -11,6 +11,8 @@ if __name__ == '__main__':
     for x in db.apps.find(unpublished_query):
         url = x['link']
         cat = '#' + x['cat']
-        telegram.send_app(url, cat)
+        sent = telegram.send_app(url, cat)
         time.sleep(2)
-        db.apps.update_one({'link': url}, set_published_query)
+        if sent.status_code == 200:
+            db.apps.update_one({'link': url}, set_published_query)
+            time.sleep(3)
